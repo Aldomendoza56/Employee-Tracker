a1738f759f12a7112d92eeaf6fc72c284fa160f1
Aldo Mendoza <aldomendoza56@gmail.com>
2024-03-02 11:54:22 -0800

    finished making db

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,12 @@ CREATE TABLE role (
     departmentId INT NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    roleId INT NOT NULL,
+    managerId INT,
+    PRIMARY KEY (id)
+);
